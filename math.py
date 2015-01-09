995880d9537dc65732e0c50c65798b256240ad66
songbo <sbo@zju.edu.cn>
2015-01-09 15:29:08 +0800

    init, add perm

---
+++ math.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#encoding:utf-8
+#author: Song Bo
+def perm(n, m):
+	"""return a list of all the permutations of A^n_m
+	   n and m should be less than 7 due to the calculation efficiency.
+		"""
+	if m == 1:
+		return [[x] for x in xrange(1, n + 1)]
+	elif n == m:		
+		return [x[0: i] + [m] + x[i: ] for x in perm(n - 1, m - 1) for i in xrange(0, len(x) + 1)]
+	else:
+		return perm(n, m - 1) + [x[0: i] + [m] + x[i: ] for x in perm(n - 1, m - 1) for i in xrange(0, len(x) + 1)]
+
